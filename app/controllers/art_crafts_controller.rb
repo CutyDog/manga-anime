19af66177b916e2df7dd96881cb4a0f3690d257e
Yusuke Yasuo <yasuo.y@dely.jp>
2020-11-28 12:57:52 +0000

    コメント表示

--- app/controllers/art_crafts_controller.rb
+++ app/controllers/art_crafts_controller.rb
@@ -2,5 +2,6 @@ class ArtCraftsController < ApplicationController
   def show
     @art_craft = ArtCraft.find(params[:id])
     @comment = Comment.new
+    @comments = @art_craft.comments.order(id: :desc)
   end
 end
