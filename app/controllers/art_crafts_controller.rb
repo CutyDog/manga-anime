2e21175bb4e28794d8f52630a977fd353f06381b
Yusuke Yasuo <yasuo.y@dely.jp>
2020-11-28 14:02:41 +0000

    コメントを昇順に変更

--- app/controllers/art_crafts_controller.rb
+++ app/controllers/art_crafts_controller.rb
@@ -2,6 +2,6 @@ class ArtCraftsController < ApplicationController
   def show
     @art_craft = ArtCraft.find(params[:id])
     @comment = Comment.new
-    @comments = @art_craft.comments.order(id: :desc)
+    @comments = @art_craft.comments
   end
 end
