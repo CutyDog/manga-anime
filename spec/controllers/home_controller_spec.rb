9d029e541d1087c3d5904f452f2276a5f5265c43
CutyDog <silver.soul.818@gmail.com>
2021-04-20 10:47:09 +0900

    home_controllerテスト

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  render_views
+
+  describe '#top' do
+    let(:art_craft) { create(:art_craft) }
+    
+    it "is success" do
+      get :top
+      expect(response.status).to eq(200)
+    end  
+  end
+
+end
